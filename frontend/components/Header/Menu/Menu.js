a721b664d2449a0199a1edbd7834b320ab1b336e
donovanftl <donovan_torreslan@outlook.com>
2021-01-27 11:13:30 -0600

    Se creo la form de para guardar las direcciones

--- frontend/components/Header/Menu/Menu.js
+++ frontend/components/Header/Menu/Menu.js
@@ -1,12 +1,34 @@
-import { useState } from "react";
-import { Container, Menu, Grid, Icon, Label } from "semantic-ui-react";
+import React, { useState, useEffect } from "react";
+import { Container, Menu, Grid, Icon } from "semantic-ui-react";
 import Link from "next/link";
+import { map } from "lodash";
 import BasicModal from "../../Modal/BasicModal";
 import Auth from "../../Auth";
+import useAuth from "../../../hooks/useAuth";
+import { getMeApi } from "../../../api/user";
+import { getPlatformsApi } from "../../../api/platform";
 
 export default function MenuWeb() {
+  const [platforms, setPlatforms] = useState([]);
   const [showModal, setShowModal] = useState(false);
   const [titleModal, setTitleModal] = useState("Inicia sesión");
+  const [user, setUser] = useState(undefined);
+  const { auth, logout } = useAuth();
+
+  useEffect(() => {
+    (async () => {
+      const response = await getMeApi(logout);
+      setUser(response);
+    })();
+  }, [auth]);
+
+  useEffect(() => {
+    (async () => {
+      const response = await getPlatformsApi();
+      console.log(response);
+      setPlatforms(response || []);
+    })();
+  }, []);
 
   const onShowModal = () => setShowModal(true);
   const onCloseModal = () => setShowModal(false);
@@ -16,10 +38,16 @@ export default function MenuWeb() {
       <Container>
         <Grid>
           <Grid.Column className="menu__left" width={6}>
-            <MenuPlatforms />
+            <MenuPlatforms platforms={platforms} />
           </Grid.Column>
           <Grid.Column className="menu__right" width={10}>
-            <MenuOptions onShowModal={onShowModal} />
+            {user !== undefined && (
+              <MenuOptions
+                onShowModal={onShowModal}
+                user={user}
+                logout={logout}
+              />
+            )}
           </Grid.Column>
         </Grid>
       </Container>
@@ -35,30 +63,61 @@ export default function MenuWeb() {
   );
 }
 
-function MenuPlatforms() {
+function MenuPlatforms(props) {
+  const { platforms } = props;
+
   return (
     <Menu>
-      <Link href="/play-station">
-        <Menu.Item as="a">PlayStation</Menu.Item>
-      </Link>
-      <Link href="/xbox">
-        <Menu.Item as="a">Xbox</Menu.Item>
-      </Link>
-      <Link href="/switch">
-        <Menu.Item as="a">Switch</Menu.Item>
-      </Link>
+      {map(platforms, (platforms) => (
+        <Link href={`/games/${platform.url}`} key={platforms._id}>
+          <Menu.Item as="a" name={platform.url}>
+            {platform.title}
+          </Menu.Item>
+        </Link>
+      ))}
     </Menu>
   );
 }
 
 function MenuOptions(props) {
-  const { onShowModal } = props;
+  const { onShowModal, user, logout } = props;
   return (
     <Menu>
-      <Menu.Item onClick={onShowModal}>
-        <Icon name="user outline" />
-        Mi cuenta
-      </Menu.Item>
+      {user ? (
+        <>
+          <Link href="/orders">
+            <Menu.Item as="a">
+              <Icon name="game" />
+              Mis pedidos
+            </Menu.Item>
+          </Link>
+          <Link href="/wishlist">
+            <Menu.Item as="a">
+              <Icon name="heart outline" />
+              Wishlist
+            </Menu.Item>
+          </Link>
+          <Link href="/account">
+            <Menu.Item as="a">
+              <Icon name="user outline" />
+              {user.name} {user.lastname}
+            </Menu.Item>
+          </Link>
+          <Link href="/cart">
+            <Menu.Item as="a" className="m-0">
+              <Icon name="cart" />
+            </Menu.Item>
+          </Link>
+          <Menu.Item className="m-0" onClick={logout}>
+            <Icon name="power off" />
+          </Menu.Item>
+        </>
+      ) : (
+        <Menu.Item onClick={onShowModal}>
+          <Icon name="user outline" />
+          Mi cuenta
+        </Menu.Item>
+      )}
     </Menu>
   );
 }
